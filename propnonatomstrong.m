ac127b1cdc370a2435e99ca869ff2ea66699c24f
Ferran <ferran@aisle51.net>
2015-07-26 16:29:48 -0700

    Imported user code snippets

---
+++ propnonatomstrong.m
@@ -0,0 +1,10 @@
+// propnonatomstrong
+// just a normal property
+//
+// IDECodeSnippetCompletionPrefix: propnonatomstrong
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 26E7F814-55BA-4D8E-9B0D-5CB35DC1A815
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+@property (nonatomic,strong) 
